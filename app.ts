81889a1d01f055042cacb0ee3dba57fb9946b382
chiharakenta <chiharakenta0519@gmail.com>
2022-12-18 16:13:50 +0900

    study: Tuple type

--- app.ts
+++ app.ts
@@ -2,11 +2,22 @@
 //   name: string;
 //   age: number;
 // }
-const person = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: Array<string>;
+  role: [number, string];
+} = {
   name: "kenta",
   age: 24,
   hobbies: ["Sports", "Cooking"],
+  role: [2, "author"],
 };
+
+// person.role.push("admin"); // pushの場合は、Tupleでもエラー検知できない。
+// person.role[1] = 10;
+
+// person.role = [0, 'admin', 'user'];
 
 let favoriteActivities: string[];
 favoriteActivities = ["sports"];
