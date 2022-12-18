5bdeda9ec998dc94b5b027126f25df8452d60c7b
chiharakenta <chiharakenta0519@gmail.com>
2022-12-18 16:20:12 +0900

    study: Enum type

--- app.ts
+++ app.ts
@@ -2,16 +2,34 @@
 //   name: string;
 //   age: number;
 // }
-const person: {
-  name: string;
-  age: number;
-  hobbies: Array<string>;
-  role: [number, string];
-} = {
+
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: Array<string>;
+//   role: [number, string];
+// } = {
+//   name: "kenta",
+//   age: 24,
+//   hobbies: ["Sports", "Cooking"],
+//   role: [2, "author"],
+// };
+
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+enum Role {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR,
+}
+
+const person = {
   name: "kenta",
   age: 24,
   hobbies: ["Sports", "Cooking"],
-  role: [2, "author"],
+  role: Role.ADMIN,
 };
 
 // person.role.push("admin"); // pushの場合は、Tupleでもエラー検知できない。
@@ -25,5 +43,9 @@ favoriteActivities = ["sports"];
 console.log(person.name);
 
 for (const hobby of person.hobbies) {
-  console.log(hobby.toUpperCase());
+  console.log(hobby);
+}
+
+if (person.role === Role.ADMIN) {
+  console.log("ADMIN User");
 }
