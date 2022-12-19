bef1295cf91450bc8fc5679fd8be5ab5591865d9
chiharakenta <chiharakenta0519@gmail.com>
2022-12-20 06:51:04 +0900

    study: tsconfig, "lib"

--- 3_settings-and-compiler/app.ts
+++ 3_settings-and-compiler/app.ts
@@ -1,6 +1,4 @@
-'use strict';
-let age: number;
-age = 30;
-
-const userName = 'Maximilian';
-console.log(userName);
+const button = document.querySelector('button')!;
+button.addEventListener('click', () => {
+  console.log('Clicked!');
+});
