467113fb2f9d67a1b26dc94e601366d560b955d4
chiharakenta <chiharakenta0519@gmail.com>
2022-12-21 14:48:46 +0900

    study(class): public, private

--- 5_class_and_interface/src/app.ts
+++ 5_class_and_interface/src/app.ts
@@ -1,5 +1,6 @@
 class Department {
   name: string;
+  private employees: Array<string> = [];
 
   constructor(n: string) {
     this.name = n;
@@ -8,12 +9,28 @@ class Department {
   describe(this: Department) {
     console.log('Department: ' + this.name);
   }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
 
 const accounting = new Department('Accounting');
 
+accounting.addEmployee('Max');
+accounting.addEmployee('Manu');
+
+// accounting.employees[2] = 'Anna';
+accounting.name = 'New Name';
+
 accounting.describe();
+accounting.printEmployeeInformation();
 
-const accountingCopy = { name: 'Dummy', describe: accounting.describe };
+// const accountingCopy = { name: 'Dummy', describe: accounting.describe };
 
-accountingCopy.describe();
+// accountingCopy.describe();
