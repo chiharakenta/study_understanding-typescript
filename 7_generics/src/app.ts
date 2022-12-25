a85eb56a6afeecad3c7609aa9f3f3ad8e2540d07
chiharakenta <chiharakenta0519@gmail.com>
2022-12-25 18:42:06 +0900

    study(generics): what is Generic Type

--- 7_generics/src/app.ts
+++ 7_generics/src/app.ts
@@ -1,1 +1,12 @@
-// Code goes here!
+const names: Array<string> = [];
+// names[0].split(' ');
+
+const promise = new Promise<number>((resolve, reject) => {
+  setTimeout(() => {
+    resolve(20);
+  }, 2000);
+});
+
+promise.then((data) => {
+  // data.split(' ');
+});
