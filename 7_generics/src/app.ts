46570dc933c8b553635f12ac62a062a52a9936d6
chiharakenta <chiharakenta0519@gmail.com>
2022-12-25 19:43:24 +0900

    study(generics): create Generic function

--- 7_generics/src/app.ts
+++ 7_generics/src/app.ts
@@ -1,12 +1,19 @@
-const names: Array<string> = [];
-// names[0].split(' ');
+// const names: Array<string> = [];
+// // names[0].split(' ');
 
-const promise = new Promise<number>((resolve, reject) => {
-  setTimeout(() => {
-    resolve(20);
-  }, 2000);
-});
+// const promise = new Promise<number>((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve(20);
+//   }, 2000);
+// });
 
-promise.then((data) => {
-  // data.split(' ');
-});
+// promise.then((data) => {
+//   // data.split(' ');
+// });
+
+const merge = <T extends {}, U>(objA: T, objB: U) => {
+  return Object.assign(objA, objB);
+};
+
+const mergedObject = merge({ name: 'Max', hobbies: ['Sports'] }, { age: 30 });
+console.log(mergedObject.age);
