83d34e17f147a8b2879ef2c5a1eb18e139bd3c1c
chiharakenta <chiharakenta0519@gmail.com>
2022-12-26 02:58:54 +0900

    study(generics): Generic class

--- 7_generics/src/app.ts
+++ 7_generics/src/app.ts
@@ -39,3 +39,36 @@ const extractAndConvert = <T extends object, U extends keyof T>(
 };
 
 extractAndConvert({ name: 'Max' }, 'name');
+
+class DataStorage<T extends string | number | boolean> {
+  private data: Array<T> = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    if (this.data.indexOf(item) === -1) return;
+    this.data.splice(this.data.indexOf(item), 1); // -1 参照型で探すと見つからずに、-1を返す→最後の要素が削除される。
+  }
+
+  getItems() {
+    return [...this.data];
+  }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem('Data1');
+textStorage.addItem('Data2');
+textStorage.removeItem('Data2');
+console.log(textStorage.getItems());
+
+const numberStorage = new DataStorage<number>();
+
+// const objStorage = new DataStorage<object>();
+// const obj = { name: 'Max' };
+// objStorage.addItem(obj);
+// objStorage.addItem({ name: 'Manu' });
+// // ...
+// objStorage.removeItem(obj);
+// console.log(objStorage.getItems());
