c7fa5e37dd9d0bad00217db024fba3586d1ad984
chiharakenta <chiharakenta0519@gmail.com>
2022-12-25 19:47:47 +0900

    study(generics): add restriction

--- 7_generics/src/app.ts
+++ 7_generics/src/app.ts
@@ -11,9 +11,9 @@
 //   // data.split(' ');
 // });
 
-const merge = <T extends {}, U>(objA: T, objB: U) => {
+const merge = <T extends object, U extends object>(objA: T, objB: U) => {
   return Object.assign(objA, objB);
 };
 
 const mergedObject = merge({ name: 'Max', hobbies: ['Sports'] }, { age: 30 });
-console.log(mergedObject.age);
+console.log(mergedObject);
