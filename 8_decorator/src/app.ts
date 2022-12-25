4e799ad43ff1d3c2fb97d8acd8dcbea19e3a5ac9
chiharakenta <chiharakenta0519@gmail.com>
2022-12-26 04:19:38 +0900

    study(decorator): accessor & parameter decorator

--- 8_decorator/src/app.ts
+++ 8_decorator/src/app.ts
@@ -40,11 +40,37 @@ const Log = (target: any, propertyName: string | Symbol) => {
   console.log(target, propertyName);
 };
 
+const Log2 = (target: any, name: string, descriptor: PropertyDescriptor) => {
+  console.log('Accessor デコレータ');
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+};
+
+const Log3 = (
+  target: any,
+  name: string | Symbol,
+  descriptor: PropertyDescriptor
+) => {
+  console.log('Method デコレータ');
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+};
+
+const Log4 = (target: any, name: string | Symbol, position: number) => {
+  console.log('Parameter デコレータ');
+  console.log(target);
+  console.log(name);
+  console.log(position);
+};
+
 class Product {
   @Log
   title: string;
   private _price: number;
 
+  @Log2
   set price(val: number) {
     if (val <= 0) throw new Error('不正な価格です - 0以下は設定できません。');
     this._price = val;
@@ -55,7 +81,8 @@ class Product {
     this._price = p;
   }
 
-  getPriceWithTax(taxRate: number) {
+  @Log3
+  getPriceWithTax(@Log4 taxRate: number) {
     return this._price * (1 + taxRate);
   }
 }
