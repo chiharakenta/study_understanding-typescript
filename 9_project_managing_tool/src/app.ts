817f676fcd6225661c6c0afb70dcd588741a9ccb
chiharakenta <chiharakenta0519@gmail.com>
2023-01-16 16:53:28 +0900

    study(project-managing-tool): DOM manipulation

--- 9_project_managing_tool/src/app.ts
+++ 9_project_managing_tool/src/app.ts
@@ -2,6 +2,9 @@ class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   formElement: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  mandayInputElement: HTMLInputElement;
 
   constructor() {
     this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
@@ -9,7 +12,23 @@ class ProjectInput {
 
     const importedNode = document.importNode(this.templateElement.content, true);
     this.formElement = importedNode.firstElementChild as HTMLFormElement;
+    this.formElement.id = 'user-input';
+
+    this.titleInputElement = this.formElement.querySelector('#title') as HTMLInputElement;
+    this.descriptionInputElement = this.formElement.querySelector('#description') as HTMLInputElement;
+    this.mandayInputElement = this.formElement.querySelector('#manday') as HTMLInputElement;
+
+    this.configure();
     this.attach();
+  }
+
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+
+  private configure() {
+    this.formElement.addEventListener('submit', this.submitHandler.bind(this));
   }
 
   private attach() {
