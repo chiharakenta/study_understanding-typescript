88b449b9bdfb6f36918994259f954dc81af43dd9
chiharakenta <chiharakenta0519@gmail.com>
2023-01-18 10:54:26 +0900

    study(project-managing-tool): project filtering

--- 9_project_managing_tool/src/app.ts
+++ 9_project_managing_tool/src/app.ts
@@ -114,7 +114,13 @@ class ProjectList {
     this.element.id = `${this.type}-projects`;
 
     projectState.addListener((projects: Array<Project>) => {
-      this.assignedProjects = projects;
+      const relevantProjects = projects.filter((prj) => {
+        if (this.type === 'active') {
+          return prj.status === ProjectStatus.Active;
+        }
+        return prj.status === ProjectStatus.Finished;
+      });
+      this.assignedProjects = relevantProjects;
       this.renderProjects();
     });
 
@@ -124,6 +130,7 @@ class ProjectList {
 
   private renderProjects() {
     const listEl = document.getElementById(`${this.type}-projects-list`)! as HTMLUListElement;
+    listEl.innerHTML = '';
     for (const prjItem of this.assignedProjects) {
       const listItem = document.createElement('li');
       listItem.textContent = prjItem.title;
